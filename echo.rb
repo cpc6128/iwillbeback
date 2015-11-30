72a5f7f56dbb8968e960296e570cd2822d9728d8
cpc <ronan.mounes@gmail.com>
2015-11-30 14:13:31 -0800

    ajout du script

---
+++ echo.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+######################################
+# Auteur: Ronan Mounès <ronan.mounes@gmail.com>
+# Mise à jour le: 27/11/2015
+######################################
+puts '################################################'
+puts 'Au plaisir de travailler à nouveau ensemble !'
+puts 'mon courriel : ronan.mounes@gmail.com'
+puts ''
+puts '                       ______'
+puts '                     <((((((\\\\\\'
+puts '                     /      . }\\'
+puts '                     ;--..--._|}'
+puts '                     \'--/\\--\'  )'
+puts '                     | \'-\'  :\'|'
+puts '                     . -==- .-|'
+puts '                      \\.__.\'   \\--._'
+puts '                  __.--|       //  _/\'--.'
+puts '                .\'-._ (\'-----\'/ __/      \\'
+puts '               /   __>|      | \'--.       |'
+puts '              |   \\   |     /    /       /'
+puts '             /     \\  |     |  _/       /'
+puts '            \\       \\ |     | /        /'
+puts '             \\      \\        /'
+
